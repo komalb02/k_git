cd06535e69ed296576524ca718304af486c2bccb
Suparna-Som <suparna@futurewise.co.in>
2022-06-20 12:55:31 +0530

    area of triangle

---
+++ src/main/java/aot.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class aot {
+    public static void main(String[] args)
+    {
+        Scanner sn= new Scanner(System.in);
+        System.out.println("Enter base of triangle:");
+        double base = sn.nextDouble();
+        System.out.println("Enter base of triangle");
+        double height = sn.nextDouble();
+
+        aot at=new aot();
+        double area = at.calculateArea(base,height);
+        System.out.println("Area of triangle is:"+area);
+    }
+    private double calculateArea(double base, double height)
+    {
+        return base*height/2;
+    }
+}
+
+
